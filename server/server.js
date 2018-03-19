f3867ae9e01e6456e43c7cb6670291039c9f3275
marclou <marc.louvion@gmail.com>
2018-03-19 16:08:58 +0900

    REST API available

--- server/server.js
+++ server/server.js
@@ -1,6 +1,27 @@
-const { mongoose } = require('./db/mongoose.js');
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const { mongoose } = require('./db/mongoose.js');
 const { Todo } = require('./models/Todo.js');
 const { User } = require('./models/User.js');
 
-console.log(Todo);
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+     const { username, email } = req.body;
+     const user = new User({
+          username: username,
+          email: email
+     });
+     user.save().then((doc) => {
+          res.send(doc);
+     }, (e) => {
+          res.status(400).send(e);
+     })
+});
+
+app.listen('3000', () => {
+     console.log('Listen on port 3000 \n');
+})
