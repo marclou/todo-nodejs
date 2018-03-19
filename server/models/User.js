78269b1e0bd4baab1ba5a975974063f9092c0f47
marclou <marc.louvion@gmail.com>
2018-03-19 15:18:18 +0900

    adding mongodb schemas

---
+++ server/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User', {
+     username: {
+          type: String,
+          required: true,
+          trim: true,
+          minlength: 3,
+          maxlength: 7
+     },
+     email: {
+          type: String,
+          required: true,
+          trim: true
+     },
+     age: {
+          type: Number,
+          default: undefined
+     }
+});
+
+module.exports = {
+     User
+};
---
+++ server/server.js
@@ -0,0 +1,6 @@
+const { mongoose } = require('./db/mongoose.js');
+
+const { Todo } = require('./models/Todo.js');
+const { User } = require('./models/User.js');
+
+console.log(Todo);
